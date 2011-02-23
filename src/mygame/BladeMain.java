df05ca518bc6ab08857bebb06cd0969cc4764c65
killergift <killergift@gmail.com>
2011-02-23 13:53:15 -0800

    test change,

--- src/mygame/BladeMain.java
+++ src/mygame/BladeMain.java
@@ -4,6 +4,7 @@ public class BladeMain {
     public static void main(String[] args) {
         if(args.length==0){
             System.out.println("Please indicate usage as 'server' or 'client' in first argument.");
+       
         }
         else if(args[0].toLowerCase().equals("server")){
             BladeServer.main(args);
