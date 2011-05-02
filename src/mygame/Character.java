6ca10c6bf5864ca953aa6a84a3a9408585b9b15b
killergift <killergift@gmail.com>
2011-05-02 03:51:58 -0700

    ok...

--- src/mygame/Character.java
+++ src/mygame/Character.java
@@ -8,6 +8,7 @@ package mygame;
 import com.jme3.animation.AnimControl;
 import com.jme3.animation.Bone;
 import com.jme3.asset.AssetManager;
+import com.jme3.asset.DesktopAssetManager;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
@@ -25,74 +26,134 @@ import com.jme3.scene.Node;
  * @author blah
  */
 public class Character{
-    static public Node createCharacter(String bodyAssetPath,String swordAssetPath,AssetManager assetManager,BulletAppState bulletAppState,boolean applyPhysics, long playerID){
+    Node bodyModel;
+    Node swordModel;
+    
+    SwordControl swordControl;
+    BodyControl bodyControl;
+    CharacterControl charControl;
+    
+    Vector3f upperArmAngles;
+    Vector3f upperArmVels;
+    Vector3f position;
+    Vector3f velocity;
+    
+    Vector3f upperArmDeflectVels;
+    
+    float charAngle;
+    float turnVel;
+    float elbowWristAngle;
+    float elbowWristVel;
+    
+    Long playerID;
+    
+    public Character(Long playerID,BulletAppState bulletAppState, AssetManager assetManager){
+        this.playerID=playerID;
         
-        Node bodyModel=(Node)assetManager.loadModel(bodyAssetPath);
+        bodyModel=(Node)assetManager.loadModel("Models/Female.mesh.j3o");
         bodyModel.scale(1.0f, 1.0f, 1.0f);
         bodyModel.rotate(0.0f, FastMath.HALF_PI, 0.0f);
         bodyModel.setLocalTranslation(0.0f, 20.0f, 0.0f);
-        bodyModel.setName(Long.toString(playerID));
         
-        Node swordModel=(Node)assetManager.loadModel(swordAssetPath);
+        swordModel=(Node)assetManager.loadModel("Models/sword.mesh.j3o");
         swordModel.scale(1.0f, 1.0f, 1.0f);
         swordModel.setName("sword");
+        
         bodyModel.attachChild(swordModel);
         
-        if (applyPhysics) {
-            CapsuleCollisionShape capsule = new CapsuleCollisionShape(1f, 4.5f);
-
-            /*
-            Mesh mesh = findMesh(model);
-            CollisionShape gimpact = new GImpactCollisionShape(mesh);
-            RigidBodyControl rigidControl = new RigidBodyControl(gimpact, 0.01f);
-            rigidControl.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-            rigidControl.removeCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_01);
-            rigidControl.addCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-            rigidControl.setKinematic(true);
-            model.addControl(rigidControl);
-             */
-            //bulletAppState.getPhysicsSpace().add(rigidControl);
-            
-            // create collision shape for sword
-            Bone hand = bodyModel.getControl(AnimControl.class).getSkeleton().getBone("swordHand");
-            Matrix3f rotation = hand.getModelSpaceRotation().toRotationMatrix();
-            Vector3f position = hand.getModelSpacePosition();
-            
-            swordModel.setLocalRotation(rotation);
-            swordModel.setLocalTranslation(position);
-            
-            //swordModel.getControl(AnimControl.class).getSkeleton().getBone("swordBone").getModelSpacePosition();
-            //swordModel.setLocalTranslation(position);
-            
-            Vector3f shiftPosition = rotation.mult(new Vector3f(0f, 0f, 2.5f));
-            CompoundCollisionShape cShape = new CompoundCollisionShape();
-            Vector3f boxSize = new Vector3f(.1f, .1f, 2.25f);
-            cShape.addChildShape(new BoxCollisionShape(boxSize), position, rotation);
-            CollisionShapeFactory.shiftCompoundShapeContents(cShape, shiftPosition);
-            
-            // create control for sword
-            SwordControl sword = new SwordControl(cShape, playerID);
-            sword.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-            sword.removeCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_01);
-            sword.addCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-             
-            // create control for body
-            BodyControl body = new BodyControl(capsule, playerID);
-            body.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-            body.removeCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_01);
-            body.addCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
-            
-            // create control for character
-            CharacterControl charControl = new CharacterControl(capsule, 0.01f);
-            
-            swordModel.addControl(sword);
-            bodyModel.addControl(body);
-            bodyModel.addControl(charControl);
-            
-            bulletAppState.getPhysicsSpace().add(sword);
-            bulletAppState.getPhysicsSpace().add(body);
-            bulletAppState.getPhysicsSpace().add(charControl);
+        upperArmDeflectVels=new Vector3f();
+        upperArmAngles=new Vector3f();
+        upperArmVels=new Vector3f();
+        position=new Vector3f();
+        velocity=new Vector3f();
+        charAngle=0f;
+        turnVel=0f;
+        elbowWristAngle=CharMovement.Constraints.lRotMin;
+        elbowWristVel=0f;
+        
+        applyPhysics(bulletAppState);
+    }
+    
+    public void update(float tpf,boolean serverChar){
+        upperArmAngles=CharMovement.extrapolateUpperArmAngles(upperArmAngles, upperArmVels, upperArmDeflectVels, tpf);
+        elbowWristAngle=CharMovement.extrapolateLowerArmAngles(elbowWristAngle, elbowWristVel, tpf);
+        charAngle=CharMovement.extrapolateCharTurn(charAngle, turnVel, tpf);
+        CharMovement.setUpperArmTransform(upperArmAngles, bodyModel);
+        CharMovement.setLowerArmTransform(elbowWristAngle, bodyModel);
+        
+        float xDir, zDir;
+        zDir = FastMath.cos(charAngle);
+        xDir = FastMath.sin(charAngle);
+        Vector3f viewDirection = new Vector3f(xDir, 0, zDir);
+        
+        charControl.setViewDirection(viewDirection);
+        Vector3f forward, up, left;
+        float xVel, zVel;
+        xVel = velocity.x;
+        zVel = velocity.z;
+        forward = new Vector3f(viewDirection);
+        up = new Vector3f(0, 1, 0);
+        left = up.cross(forward);
+        
+        Vector3f newVelocity = left.mult(xVel).add(forward.mult(zVel));
+        
+        if(serverChar){
+            position = charControl.getPhysicsLocation();
         }
-        return bodyModel;
-    }       
+        else {
+            Vector3f extrapolatedPosition, currentPosition;
+            
+            currentPosition = bodyModel.getLocalTranslation();
+            extrapolatedPosition = position;
+            Vector3f diffVect = new Vector3f(extrapolatedPosition.x - currentPosition.x, 0, extrapolatedPosition.z - currentPosition.z);
+            float correctiveConstant = 0.2f;
+            Vector3f correctiveVelocity = new Vector3f(diffVect.x * correctiveConstant, 0, diffVect.z * correctiveConstant);
+      
+            newVelocity.addLocal(correctiveVelocity);
+        }
+        
+        charControl.setWalkDirection(newVelocity);
+    }
+    
+    private void applyPhysics(BulletAppState bulletAppState){
+        CapsuleCollisionShape capsule = new CapsuleCollisionShape(1f, 4.5f);
+
+        // create collision shape for sword
+        Bone hand = bodyModel.getControl(AnimControl.class).getSkeleton().getBone("swordHand");
+        Matrix3f rotation = hand.getModelSpaceRotation().toRotationMatrix();
+        Vector3f position = hand.getModelSpacePosition();
+
+        swordModel.setLocalRotation(rotation);
+        swordModel.setLocalTranslation(position);
+
+
+        Vector3f shiftPosition = rotation.mult(new Vector3f(0f, 0f, 2.5f));
+        CompoundCollisionShape cShape = new CompoundCollisionShape();
+        Vector3f boxSize = new Vector3f(.1f, .1f, 2.25f);
+        cShape.addChildShape(new BoxCollisionShape(boxSize), position, rotation);
+        CollisionShapeFactory.shiftCompoundShapeContents(cShape, shiftPosition);
+
+        // create control for sword
+        swordControl = new SwordControl(cShape, playerID);
+        swordControl.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
+        swordControl.removeCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_01);
+        swordControl.addCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
+
+        // create control for body
+        bodyControl = new BodyControl(capsule, playerID);
+        bodyControl.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
+        bodyControl.removeCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_01);
+        bodyControl.addCollideWithGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
+
+        // create control for character
+        charControl = new CharacterControl(capsule, 0.01f);
+
+        swordModel.addControl(swordControl);
+        bodyModel.addControl(bodyControl);
+        bodyModel.addControl(charControl);
+
+        bulletAppState.getPhysicsSpace().add(swordControl);
+        bulletAppState.getPhysicsSpace().add(bodyControl);
+        bulletAppState.getPhysicsSpace().add(charControl);
+    } 
 }
